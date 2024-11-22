9464e5364e400bee6b5721e30a84687587db7c44
tiana <tiana.randrianarivony@gmail.com>
2024-11-22 12:26:43 +0300

    static site plan init

--- test/app/components/siteplan/index.tsx
+++ test/app/components/siteplan/index.tsx
@@ -1,40 +1,46 @@
+import present from "@/public/icons/present.svg";
+import Image from "next/image";
 export default function Siteplan() {
     return (
-        <div className="mt-3 w-full bg-[#FCF5E7] flex items-center h-1/3">
-            <section className="grid gap-4 mx-auto max-w-[1200px] p-4 sm:grid-cols-2 md:grid-cols-4 h-full">
-                {/* Premier élément */}
-                <div className="flex items-center">
+        <div className="mt-3 w-full bg-[#FCF5E7] flex items-center">
+            <section
+                className="flex items-center justify-between mx-auto max-w-[960px]  p-4 w-full flex-col sm:flex-row">
+                <div className="flex flex-col items-center mb-4 sm:mb-0">
                     <div
-                        className="w-10 h-10 flex items-center justify-center rounded-full bg-blue-500 text-white font-bold">
-                        1
+                        className="w-20 h-20 flex items-center justify-center rounded-full bg-primary text-white font-bold">
+                        <Image src={present} alt="home" width={50}/>
                     </div>
-                    <span className="h-[2px] bg-gray-400 w-8 sm:w-[2px] sm:h-8 md:w-8 md:h-[2px]"></span>
+                    <span>Choisissez un bon</span>
                 </div>
 
-                {/* Deuxième élément */}
-                <div className="flex  items-center">
+                <div className="flex-1 border-t-2 border-dashed border-[#D9BBA2] mx-4 p-2"></div>
+
+                <div className="flex flex-col items-center mb-4 sm:mb-0">
                     <div
-                        className="w-10 h-10 flex items-center justify-center rounded-full bg-blue-500 text-white font-bold">
-                        2
+                        className="w-20 h-20 flex items-center justify-center rounded-full bg-primary text-white font-bold">
+                        <Image src={present} alt="home" width={50}/>
                     </div>
-                    <span className="h-[2px] bg-gray-400 w-8 sm:w-[2px] sm:h-8 md:w-8 md:h-[2px]"></span>
+                    <span>Choisissez un bon</span>
                 </div>
 
-                {/* Troisième élément */}
-                <div className="flex  items-center">
+                <div className="flex-1 border-t-2 border-dashed border-[#D9BBA2] mx-4 p-2"></div>
+
+                <div className="flex flex-col items-center mb-4 sm:mb-0">
                     <div
-                        className="w-10 h-10 flex items-center justify-center rounded-full bg-blue-500 text-white font-bold">
-                        3
+                        className="w-20 h-20 flex items-center justify-center rounded-full bg-primary text-white font-bold">
+                        <Image src={present} alt="home" width={50}/>
                     </div>
-                    <span className="h-[2px] bg-gray-400 w-8 sm:w-[2px] sm:h-8 md:w-8 md:h-[2px]"></span>
+                    <span>Choisissez un bon</span>
                 </div>
 
-                {/* Quatrième élément */}
-                <div className="flex  items-center">
+                <div className="flex-1 border-t-2 border-dashed border-[#D9BBA2] mx-4 p-2"></div>
+
+                <div className="flex flex-col items-center mb-4 sm:mb-0">
                     <div
-                        className="w-10 h-10 flex items-center justify-center rounded-full bg-blue-500 text-white font-bold">
-                        4
+                        className="w-20 h-20 flex items-center justify-center rounded-full bg-primary text-white font-bold">
+                        <Image src={present} alt="home" width={50}/>
                     </div>
+                    <span>Choisissez un bon</span>
                 </div>
             </section>
         </div>
