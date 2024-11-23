a537461d0d1340f74b45a7a9bcd29e5c3f5c3b5a
tiana <tiana.randrianarivony@gmail.com>
2024-11-23 14:20:06 +0300

    footer

--- app/components/footer/index.tsx
+++ app/components/footer/index.tsx
@@ -21,14 +21,14 @@ export default function Footer() {
             </div>
         </div>
 
-        <div className="container mx-auto mt-8">
-            <div className="grid grid-cols-1 sm:grid-cols-2 gap-6 text-xs">
-                <div className="text-center sm:text-left">
+        <div className="container mx-auto mt-8 max-w-[1024px]">
+            <div className="grid grid-cols-1 lg:grid-cols-2  gap-6 text-xs">
+                <div className="text-center ">
                     <p>Site développé par <a
                         href="mailto:tiana.randrianarivony@gmail.com">tiana.randrianarivony@gmail.com</a></p>
                 </div>
 
-                <div className="flex justify-center sm:justify-end space-x-6">
+                <div className="flex justify-center space-x-6">
                     <a href="/mentions-legales" className="hover:text-gray-400">Mentions légales</a>
                     <a href="/politique-confidentialite" className="hover:text-gray-400">Politique de
                         confidentialité</a>
